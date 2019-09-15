67439b5ebf75f1e9a906561f5a428db88c1b9ed8
Abdul Campos <acampos0297@tamu.edu>
2019-09-15 16:11:39 -0500

    Finshed Part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -25,14 +25,18 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, "+name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  /[bcdfghjklmnprstvwxyz]/i.match(s[0])
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  return true if s=='0'
+  /^[10]*00$/.match(s)
 end
 
 # Part 3
